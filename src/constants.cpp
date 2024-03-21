aec8d0116ec03cf21eee4a6602fe96365b9c9ce7
philip <44369870+pnwinkler@users.noreply.github.com>
2024-03-21 18:58:04 +0100

    Apply formatting

--- src/constants.cpp
+++ src/constants.cpp
@@ -4,21 +4,9 @@
 constexpr int NORTH = 1, SOUTH = 2, EAST = 4, WEST = 8;
 
 // The difference in x for each direction
-std::unordered_map<int, int> DX = {
-    {NORTH, 0},
-    {SOUTH, 0},
-    {EAST, 1},
-    {WEST, -1}};
+std::unordered_map<int, int> DX = {{NORTH, 0}, {SOUTH, 0}, {EAST, 1}, {WEST, -1}};
 
 // The difference in y for each direction
-std::unordered_map<int, int> DY = {
-    {NORTH, -1},
-    {SOUTH, 1},
-    {EAST, 0},
-    {WEST, 0}};
+std::unordered_map<int, int> DY = {{NORTH, -1}, {SOUTH, 1}, {EAST, 0}, {WEST, 0}};
 
-std::unordered_map<int, int> OPPOSITE = {
-    {NORTH, SOUTH},
-    {SOUTH, NORTH},
-    {EAST, WEST},
-    {WEST, EAST}};
+std::unordered_map<int, int> OPPOSITE = {{NORTH, SOUTH}, {SOUTH, NORTH}, {EAST, WEST}, {WEST, EAST}};
--- src/recursive_backtracking.cpp
+++ src/recursive_backtracking.cpp
@@ -7,14 +7,14 @@
 // see https://zig.news/perky/hot-reloading-with-raylib-4bf9 for interesting ideas
 
 // Generate a maze using the recursive backtracking algorithm and display it in the console
+#include <raylib.h>
 #include <deque>
+#include <functional>  // for std::bind
 #include <future>
-#include <functional> // for std::bind
 #include <iostream>
 #include <random>
-#include <raylib.h>
-#include "./utils.cpp"
 #include "./constants.cpp"
+#include "./utils.cpp"
 
 //------------------------------------------------------------------------------
 // Set up data structures to describe passage direction and aid with maze creation
@@ -35,8 +35,7 @@ bool firstSimulationTick = true;
 
 // Holds the X and Y locations of the most recent edit on the grid
 // Each edit can affect 0-2 locations inclusive.
-struct mostRecentGridEdit
-{
+struct mostRecentGridEdit {
     int x0;
     int x1;
     int y0;
@@ -51,10 +50,9 @@ std::deque<std::packaged_task<bool()>> taskDeque;
 // Functions for displaying the maze
 //------------------------------------------------------------------------------
 
-void simulationTick(gridType *grid)
-{
+void simulationTick(gridType* grid) {
     // Forward declarations
-    bool _carvePassagesFrom(int startX, int startY, gridType *grid);
+    bool _carvePassagesFrom(int startX, int startY, gridType* grid);
     void _simulationDraw(gridType * grid);
     void _displayMazeInConsole(gridType * grid);
 
@@ -64,8 +62,7 @@ void simulationTick(gridType *grid)
     // contains tasks which don't change the grid's state.
     auto changeEffected = false;
 
-    if (firstSimulationTick)
-    {
+    if (firstSimulationTick) {
         firstSimulationTick = false;
         _simulationDraw(grid);
         _displayMazeInConsole(grid);
@@ -73,8 +70,7 @@ void simulationTick(gridType *grid)
         return;
     }
 
-    while (!changeEffected && !taskDeque.empty())
-    {
+    while (!changeEffected && !taskDeque.empty()) {
         std::packaged_task<bool()> task = std::move(taskDeque.front());
         taskDeque.pop_front();
         auto fut = task.get_future();
@@ -83,28 +79,24 @@ void simulationTick(gridType *grid)
 
         // All tasks have finished running. We do one last display update,
         // to update the task count in the graphical window
-        if (taskDeque.empty())
-        {
+        if (taskDeque.empty()) {
             _simulationDraw(grid);
         }
     }
 
     // We only need to update the display if the grid's state has changed
-    if (changeEffected)
-    {
+    if (changeEffected) {
         _simulationDraw(grid);
         _displayMazeInConsole(grid);
     }
 }
 
-void displayMaze(gridType *grid)
-{
+void displayMaze(gridType* grid) {
     // Display the maze graphically and in console
     InitWindow(xPixels, yPixels, "Maze");
     SetTargetFPS(fps);
 
-    while (!WindowShouldClose())
-    {
+    while (!WindowShouldClose()) {
         BeginDrawing();
         simulationTick(grid);
         EndDrawing();
@@ -112,16 +104,13 @@ void displayMaze(gridType *grid)
     CloseWindow();
 }
 
-void _simulationDraw(gridType *grid)
-{
+void _simulationDraw(gridType* grid) {
     // Helper function, to draw grid state in GUI. Expects an existing window.
 
     ClearBackground(RAYWHITE);
     DrawText(("Tasks " + std::to_string(taskDeque.size())).c_str(), 10, 10, 10, MAROON);
-    for (int y = 0; y < grid->size(); y++)
-    {
-        for (int x = 0; x < grid->at(0).size(); x++)
-        {
+    for (int y = 0; y < grid->size(); y++) {
+        for (int x = 0; x < grid->at(0).size(); x++) {
             int val = grid->at(y).at(x);
 
             if (val != SOUTH && !(y < grid->size() - 1 && grid->at(y + DY[SOUTH])[x] == NORTH))
@@ -137,8 +126,7 @@ void _simulationDraw(gridType *grid)
     }
 }
 
-void _displayMazeInConsole(gridType *grid)
-{
+void _displayMazeInConsole(gridType* grid) {
     // Helper function. Prints the maze to the console. Can be called directly,
     // but is generally expected to be called indirectly by a function that prompts
     // grid state updates.
@@ -146,19 +134,17 @@ void _displayMazeInConsole(gridType *grid)
     // Create top wall
     int height = grid->size();
     int width = grid->at(0).size();
-    for (int i = 0; i < width * 2; i++)
-    {
+    for (int i = 0; i < width * 2; i++) {
         std::cout << '_';
     }
     std::cout << '\n';
 
-    for (int y = 0; y < height; y++)
-    {
+    for (int y = 0; y < height; y++) {
         std::cout << '|';
-        for (int x = 0; x < width; x++)
-        {
+        for (int x = 0; x < width; x++) {
             int val = grid->at(y).at(x);
-            (val == SOUTH || (y < height - 1 && grid->at(y + DY[SOUTH])[x] == NORTH)) ? std::cout << ' ' : std::cout << '_';
+            (val == SOUTH || (y < height - 1 && grid->at(y + DY[SOUTH])[x] == NORTH)) ? std::cout << ' '
+                                                                                      : std::cout << '_';
             (val == EAST || (x < width - 1 && grid->at(y)[x + DX[EAST]] == WEST)) ? std::cout << ' ' : std::cout << '|';
         }
         std::cout << '\n';
@@ -170,9 +156,8 @@ void _displayMazeInConsole(gridType *grid)
 // The algorithm itself
 //------------------------------------------------------------------------------
 
-bool _carvingHelper(int startX, int startY, int newX, int newY, int direction, gridType *grid);
-bool _carvePassagesFrom(int startX, int startY, gridType *grid)
-{
+bool _carvingHelper(int startX, int startY, int newX, int newY, int direction, gridType* grid);
+bool _carvePassagesFrom(int startX, int startY, gridType* grid) {
     // Connects two cells in the grid, subject to constraints.
     // Intended to be called indirectly.
 
@@ -182,15 +167,12 @@ bool _carvePassagesFrom(int startX, int startY, gridType *grid)
     std::mt19937 g(rd());
 
     std::shuffle(directions.begin(), directions.end(), g);
-    for (const auto &direction : directions)
-    {
+    for (const auto& direction : directions) {
         int newX = startX + DX[direction];
         int newY = startY + DY[direction];
 
-        bool targetInBounds = newY >= 0 && newY < (int)grid->size() &&
-                              newX >= 0 && newX < (int)grid->at(0).size();
-        if (targetInBounds && grid->at(newY).at(newX) == 0)
-        {
+        bool targetInBounds = newY >= 0 && newY < (int)grid->size() && newX >= 0 && newX < (int)grid->at(0).size();
+        if (targetInBounds && grid->at(newY).at(newX) == 0) {
             // Queueing tasks lets us more easily control the interval between simulation
             // steps, which makes rendering the state easier
             std::packaged_task<bool()> task(std::bind(_carvingHelper, startX, startY, newX, newY, direction, grid));
@@ -201,48 +183,45 @@ bool _carvePassagesFrom(int startX, int startY, gridType *grid)
     return false;
 }
 
-bool _carvingHelper(int startX, int startY, int newX, int newY, int direction, gridType *grid)
-{
+bool _carvingHelper(int startX, int startY, int newX, int newY, int direction, gridType* grid) {
     // Connect source and target cells. Returns true if it changed something, else false.
     // Having this as a separate function allows us to have one pop of the task queue correspond
     // to 0-1 updates of the board's state.
 
     // auto DEBUG_1 = grid->at(startY).at(startX);
     // auto DEBUG_2 = grid->at(newY).at(newX);
-    bool targetInBounds = newY >= 0 && newY < (int)grid->size() &&
-                          newX >= 0 && newX < (int)grid->at(0).size();
+    bool targetInBounds = newY >= 0 && newY < (int)grid->size() && newX >= 0 && newX < (int)grid->at(0).size();
     bool cond = targetInBounds && grid->at(newY).at(newX) == 0;
 
-    if (cond)
-    {
+    if (cond) {
         mrge.y0 = -1;
         mrge.x0 = -1;
         mrge.x1 = -1;
         mrge.y1 = -1;
 
         // Don't overwrite existing connections
-        if (grid->at(startY).at(startX) == 0)
-        {
+        if (grid->at(startY).at(startX) == 0) {
             grid->at(startY).at(startX) = direction;
             mrge.x0 = startX;
             mrge.y0 = startY;
-            // std::cout << "Debug: changed start (" << startY << "," << startX << ") from " << DEBUG_1 << " to " << direction << '\n';
+            // std::cout << "Debug: changed start (" << startY << "," << startX << ") from " << DEBUG_1 << " to " <<
+            // direction << '\n';
         }
         grid->at(newY).at(newX) = OPPOSITE[direction];
         mrge.x1 = newX;
         mrge.y1 = newY;
-        // std::cout << "Debug: changed target (" << newY << "," << newX << ") from " << DEBUG_2 << " to " << OPPOSITE[direction] << std::endl;
+        // std::cout << "Debug: changed target (" << newY << "," << newX << ") from " << DEBUG_2 << " to " <<
+        // OPPOSITE[direction] << std::endl;
     }
     std::packaged_task<bool()> task(std::bind(_carvePassagesFrom, newX, newY, grid));
     taskDeque.push_front(std::move(task));
     return cond;
 }
 
-int main()
-{
-    if (xPixels % cellWidth != 0 || yPixels % cellHeight != 0)
-    {
-        std::cout << "Pixel dimensions (" << xPixels << ", " << yPixels << ") cannot be neatly divided by cell dimensions."
+int main() {
+    if (xPixels % cellWidth != 0 || yPixels % cellHeight != 0) {
+        std::cout << "Pixel dimensions (" << xPixels << ", " << yPixels
+                  << ") cannot be neatly divided by cell dimensions."
                   << " This may result in a maze that doesn't neatly fit the screen\n";
     }
 
