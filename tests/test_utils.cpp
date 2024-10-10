133c93a1804e4efcda21166ca5920a1cbcfae0d2
philip <44369870+pnwinkler@users.noreply.github.com>
2024-10-10 23:58:20 +0200

    Add test for accessible neighbor function

--- tests/test_utils.cpp
+++ tests/test_utils.cpp
@@ -1,6 +1,7 @@
 #include <cassert>  // for assert
 #include <cstdlib>  // for std::abort
 #include <iostream>
+#include "../src/constants.cpp"
 #include "../src/utils.h"
 
 int testCreateEmptyGrid() {
@@ -11,6 +12,7 @@ int testCreateEmptyGrid() {
     assert(grid.at(0).at(0) == 0);
     assert(grid.at(3).at(2) == 0);
     assert(grid.at(4).at(4) == 0);
+
     return 0;
 }
 
@@ -64,7 +66,7 @@ int testGradateColor() {
     assert(res_2.b == c1.b);
     assert(res_2.a == c1.a);
 
-    // If the start color is not found at the maximum index, then its color should change to 
+    // If the start color is not found at the maximum index, then its color should change to
     // be more like the end color
     Color res_3 = utils::gradateColor(c1, c2, 1, 2);
     assert(res_3.r > c1.r);
@@ -96,13 +98,64 @@ int testGradateColor() {
     return 0;
 }
 
-// TODO: test accessible neighbors
+int testReturnAccessibleNeighbors() {
+    // When either the origin or target cell point to the other, we then consider
+    // the target cell an accessible neighbor
+    auto grid_1 = utils::createEmptyGrid(1, 2);
+    grid_1.at(0).at(0) = constants::WEST;
+    grid_1.at(0).at(1) = constants::WEST;
+    auto res_1 = utils::returnAccessibleNeighbors(grid_1, utils::XY{0, 0}, {});
+
+    // There should be 1 accessible neighbor, to the origin's right
+    assert(res_1.size() == 1);
+    auto obj_1 = res_1.at(0);
+    assert(obj_1.x == 1);
+    assert(obj_1.y == 0);
+
+    // Repeat the test, but this time, neither cell points to the other, so we expect 0 accessible neighbors
+    auto grid_2 = utils::createEmptyGrid(1, 2);
+    grid_2.at(0).at(0) = constants::WEST;
+    grid_2.at(0).at(1) = constants::EAST;
+    auto res_2 = utils::returnAccessibleNeighbors(grid_2, utils::XY{0, 0}, {});
+    assert(res_2.size() == 0);
+
+    // We expect neighbors to be accessible both horizontally and vertically. We don't test for order.
+    auto grid_3 = utils::createEmptyGrid(3, 3);
+    grid_3.at(1).at(1) = constants::WEST + constants::EAST + constants::NORTH + constants::SOUTH;
+    auto res_3 = utils::returnAccessibleNeighbors(grid_3, utils::XY{1, 1}, {});
+
+    // todo: make this less obtuse
+    // Check that our expected x and y locations all appear in the returned data structure
+    assert(res_3.size() == 4);
+    // These are the paired x, y locations that we expect to find in the results.
+    std::vector<std::tuple<int, int>> expected_neighbors = {
+        // x, y
+        {1, 0},  // Northern neighbor
+        {0, 1},  // Western neighbor
+        {2, 1},  // Eastern neighbor
+        {1, 2}   // Southern neighbor
+    };
+    for (auto&& actual : res_3) {
+        for (int i = 0; i < expected_neighbors.size(); i++) {
+            auto expected_x = std::get<0>(expected_neighbors[i]);
+            auto expected_y = std::get<1>(expected_neighbors[i]);
+            if ((expected_x == actual.x) && (expected_y == actual.y)) {
+                expected_neighbors.erase(expected_neighbors.begin() + i);
+                continue;
+            }
+        }
+    }
+    assert(expected_neighbors.size() == 0 && "Failed to pop expected elements off stack");
+
+    return 0;
+}
 
 int main() {
     testCreateEmptyGrid();
     testInBounds();
     testInBoundsXY();
     testGradateColor();
+    testReturnAccessibleNeighbors();
 
     std::cout << "All tests succeeded\n";
     return 0;
